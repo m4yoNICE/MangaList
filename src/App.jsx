e0cfb84b59d846f85a975b19a0b83efef11fb5d6
m4yoNICE <cenizaroswellrey@gmail.com>
2025-03-07 00:13:36 +0800

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header/Header.jsx";
+import MmmmFoothehehe from "./components/footer/Footer.jsx";
+import Akootenkay1point8inches from "./components/manga/Manga.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Akootenkay1point8inches />
+      <MmmmFoothehehe />
+    </>
+  );
+}
+
+export default App;
